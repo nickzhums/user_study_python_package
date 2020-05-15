6ecd050f329b9aaa8a21f59251c779f04073f135
Littlejedi <Littlejedi>
2020-05-15 15:22:24 +0800

    Install instructions

---
+++ install/install_env_track2.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+# install azure-mgmt-compute
+pip install ../../../track2_preview/azure_mgmt_compute-0.1.0-py2.py3-none-any.whl
+
+# install azure-mgmt-resource
+pip install ../../../track2_preview/azure_mgmt_resource-0.1.0-py2.py3-none-any.whl
+
+# install azure-mgmt-network
+pip install ../../../track2_preview/azure_mgmt_network-11.0.0-py2.py3-none-any.whl
+
+# pip install azure-core 1.4 (unreleased)
+pip install ../../../track2_preview/azure_core-1.4.1-py2.py3-none-any.whl
+
+# install azure-mgmt-core
+pip install ../../../track2_preview/azure_mgmt_core-1.0.0b2-py2.py3-none-any.whl
+
+# install identity
+pip install azure-identity
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Install Preview Version of Python SDK
+run `install_env_track2.sh`(in linux)   
+or you can install these packages from `packages` by yourself (as listed in the script)
