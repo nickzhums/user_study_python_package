62510f8875fb504642489fbbf5c1d3add637ad81
nickzhums <56864335+nickzhums@users.noreply.github.com>
2020-05-19 23:45:57 +0800

    Add track 1 install script

---
+++ install/install_env_track1.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+# install azure-mgmt-compute
+pip install azure_mgmt_compute
+
+# install azure-mgmt-resource
+pip install azure_mgmt_resource
+
+# install azure-mgmt-network
+pip install azure_mgmt_network
