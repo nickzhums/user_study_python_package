44c48a040554db62350a099662ade9e20f1fd840
nickzhums <56864335+nickzhums@users.noreply.github.com>
2020-05-19 23:59:34 +0800

    Commit uninstall script

---
+++ install/uninstall.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+# uninstall azure-mgmt-compute
+pip uninstall azure_mgmt_compute
+
+# uninstall azure-mgmt-resource
+pip uninstall azure_mgmt_resource
+
+# uninstall azure-mgmt-network
+pip uninstall azure_mgmt_network
