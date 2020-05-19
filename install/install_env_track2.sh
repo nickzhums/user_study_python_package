b813b920e1007839e1801c0c0842f1463f18392e
nickzhums <56864335+nickzhums@users.noreply.github.com>
2020-05-19 18:21:57 +0800

    Change order of install script

--- install/install_env_track2.sh
+++ install/install_env_track2.sh
@@ -1,4 +1,7 @@
 #/bin/bash
+# pip install azure-core 1.4 (unreleased)
+pip install azure_core-1.4.1-py2.py3-none-any.whl
+
 # install azure-mgmt-compute
 pip install azure_mgmt_compute-0.1.0-py2.py3-none-any.whl
 
@@ -7,12 +10,6 @@ pip install azure_mgmt_resource-0.1.0-py2.py3-none-any.whl
 
 # install azure-mgmt-network
 pip install azure_mgmt_network-11.0.0-py2.py3-none-any.whl
-
-# pip install azure-core 1.4 (unreleased)
-pip install azure_core-1.4.1-py2.py3-none-any.whl
-
-# install azure-mgmt-core
-pip install azure_mgmt_core-1.0.0b2-py2.py3-none-any.whl
 
 # install identity
 pip install azure-identity
